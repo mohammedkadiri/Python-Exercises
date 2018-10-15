1c4da5ed9fdf5a658d645c8b9ccb04d5ba370dc1
mohammedkadiri <mohammedkadiri72@gmail.com>
2018-10-16 00:46:56 +0100

    Add files via upload

--- List/List.py
+++ List/List.py
@@ -1,8 +1,11 @@
 # 1. Write a Python program to sum all the items in a list
 numbers = [1,2,3,4]
 total = 0
-for x in numbers:
-    total += x
-print(total)
+# for x in numbers:
+#     total += x
+# print(total)
 # 2. Write a Python program to multiplies all the items in a list.
-numbers
+total = 1
+for x in numbers:
+    total *= x
+print(total)
