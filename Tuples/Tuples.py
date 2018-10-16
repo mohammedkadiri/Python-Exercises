7a85cfb001d875fab6baa66a87c8f41968f070a6
mohammedkadiri <mohammedkadiri72@gmail.com>
2018-10-16 01:46:29 +0100

    Add files via upload

---
+++ Tuples/Tuples.py
@@ -0,0 +1,22 @@
+# 1. Write a Python program to create a tuple.
+# tuple = (1,2,3)
+# print(tuple)
+# 2. Write a Python program to create a tuple with different data types
+# tuple = (30,'String', 1, [1,2,3])
+# print(tuple)
+#3. Write a Python program to create a tuple with numbers and print one item.
+# tuple = (1,2,3)
+# tuple = 1,
+# print(tuple)
+#4. Write a Python program to unpack a tuple in several variables.
+# tuptle = (1,2,3,'hi')
+# n1, n2, n3, n4 = tuple
+# print(n1, n2, n3, n4)
+#5. Write a Python program to add an item in a tuple.
+# tuple = (1,2,3)
+# tuple = tuple + (4,)
+# print(tuple)
+#6 Write a Python program to convert a tuple to a string.
+# tuple = ('e', 'x', 'e', 'r', 'c', 'i', 's', 'e', 's')
+# str =  ''.join(tuple)
+# print(str)
