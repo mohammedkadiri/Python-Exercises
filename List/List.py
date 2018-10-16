9f827b4dfa1467ebbd104f07526abf6f379c5b3a
mohammedkadiri <mohammedkadiri72@gmail.com>
2018-10-16 01:02:42 +0100

    Add files via upload

--- List/List.py
+++ List/List.py
@@ -6,6 +6,20 @@ total = 0
 # print(total)
 # 2. Write a Python program to multiplies all the items in a list.
 total = 1
-for x in numbers:
-    total *= x
-print(total)
+# for x in numbers:
+#     total *= x
+# print
+#3. Write a Python program to get the largest number from a list
+# print(max(numbers))
+#4. Write a Python program to get the smallest number from a list.
+# print(min(numbers))
+# 5. Write a Python program to count the number of strings where the string length is 2 or more and the first and last character are same from a given list of strings.
+# sample = ['abc','xyz','aba','1221']
+# count = 0
+# for index in sample:
+#     if len(index) >= 2:
+#         temp = list(index)
+#         if temp[0] == temp[-1]:
+#             count += 1
+#
+# print("count: " + str(count))
