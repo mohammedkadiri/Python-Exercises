91ebb61946f0508e1e30271687e4c3f993d37d5e
mohammedkadiri <mohammedkadiri72@gmail.com>
2018-10-21 00:37:25 +0100

    Add files via upload

--- Dict/dict.py
+++ Dict/dict.py
@@ -32,3 +32,19 @@
 # print('Dictionary in ascending order by value : ',sorted_desc)
 # sorted_asc= sorted(d.items(), key=operator.itemgetter(0), reverse=True)
 # print('Dictionary in descending order by value : ',sorted_asc)
+
+# 2 Write a Python script to add a key to a dictionary.
+# sample =  {0: 10, 1: 20}
+#
+# sample.update({2: 30})
+#
+# print(sample)
+
+# 3 Write a Python script to concatenate following dictionaries to create a new one.
+# dic1={1:10, 2:20}
+# dic2={3:30, 4:40}
+# dic3={5:50, 6:60}
+# dict4 = {}
+# for x in (dic1,dic2,dic3):
+#     dict4.update(x)
+# print(dict4)
