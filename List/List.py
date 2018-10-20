0bac19719d32f8c0ab49fe8b1fec708143eeb504
mohammedkadiri <mohammedkadiri72@gmail.com>
2018-10-20 12:58:10 +0100

    Add files via upload

--- List/List.py
+++ List/List.py
@@ -26,7 +26,7 @@ total = 1
 #7. Write a Python program to remove duplicates from a list
 # sample = [1,2,4,1,2,4,5,3,5,6,2]
 # print(set(sample))
-# 8. Write a Python program to check a list is empty or not
+#8. Write a Python program to check a list is empty or not
 # a = []
 # if not a:
 #     if not a:
@@ -35,5 +35,33 @@ total = 1
 #     print("list is not empty")
 # 9. Write a Python program to clone or copy a list
 # sample = [1,2,4,5]
-# # copy = sample.copy()
-# # print(copy)
+#  copy = sample.copy()
+#  print(copy)
+# 10. Write a python program to find the list of words that are longer than n
+
+# def long_words(n, str):
+#     txt = str.split(" ")
+#     word = [x for x in txt if len(x) > n]
+#     return word
+#
+# print(long_words(3, "The woman said hello to the man across the street"))
+
+# 11  Write a Python function that takes two lists and returns True
+# if they have at least one common member
+
+# def compare(list_one, list_two):
+#     result = False
+#     for i in list_one:
+#         for j in list_two:
+#             if i == j:
+#                 result = True
+#                 return result
+#
+# print(compare(['hi', '1' , 'mom'],  ['1' , '2' ,'3', '5']))
+
+# 12 Write a Python program to print a specified list after removing the 0th, 4th and 5th elements
+
+# color = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+# color = [x for (i,x) in enumerate(color) if i not in (0,4,5)]
+#
+# print(color)
