b9220ec92a99cf7485f5a692f925f45a61a4f35c
mohammedkadiri <mohammedkadiri72@gmail.com>
2018-11-21 04:30:23 +0000

    created a strings folder

---
+++ String/string.py
@@ -0,0 +1,29 @@
+# Write a Python program to count the number of characters (character frequency) in a string.
+# def char_frequency(str1):
+#     dict = {}
+#     for n in str1:
+#         keys = dict.keys()
+#         if n in keys:
+#             dict[n] += 1
+#         else:
+#             dict[n] = 1
+#     return dict
+# print(char_frequency('google.com'))
+
+#Write a Python program that takes a list of words and returns the length of the longest one
+
+# words = ["orange", "yellow", "brown"]
+#
+# temp = len(words[0])
+#
+# for x in words:
+#     if len(x) > temp:
+#         temp = len(x)
+# print(temp)
+
+# Working on some string functions in python
+#convert a integer to a character
+# print(chr(100))
+#converts a character to a integer
+# print(ord('d'))
+
