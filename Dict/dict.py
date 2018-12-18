bad51f24e94ab9426123a80a0a601b572f7db235
mohammedkadiri <mohammedkadiri72@gmail.com>
2018-12-18 22:21:51 +0000

    Practicing some simple examples with files

--- Dict/dict.py
+++ Dict/dict.py
@@ -126,6 +126,7 @@
 
 # 15. Write a Python program to get the maximum and minimum value in a dictionary.
 
-my_dict = {'x':500, 'y':5874, 'z': 560}
-print(max(my_dict.values()))
-print(min(my_dict.values()))
+# my_dict = {'x':500, 'y':5874, 'z': 560}
+# print(max(my_dict.values()))
+# print(min(my_dict.values()))
+
---
+++ Files/files.py
@@ -0,0 +1,8 @@
+
+# 1. Write a Python program to read an entire text file
+
+# with open('file1.txt', 'r') as f:
+#     f_contents = f.read()
+#     print(f_contents)
+
+
