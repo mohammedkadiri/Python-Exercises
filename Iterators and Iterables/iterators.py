0e8e478783cabbab075d4d260c1aaa63d74f9825
mohammedkadiri <mohammedkadiri72@gmail.com>
2019-06-28 02:58:22 +0100

    Implemented a simple Iterator to understand iterators and iterables, also utilised generators

---
+++ Iterators and Iterables/iterators.py
@@ -0,0 +1,29 @@
+#
+# nums = [1, 2, 3]
+#
+# i_nums = iter(nums)
+#
+# print(next(i_nums))
+# print(next(i_nums))
+# print(next(i_nums))
+
+class MyRange:
+
+    def __init__(self, start, end):
+        self.value = start
+        self.end = end
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.value >= self.end:
+            raise StopIteration
+        current = self.value
+        self.value += 1
+        return  current
+
+nums = MyRange(1, 10)
+
+for num in nums:
+    print(num)
